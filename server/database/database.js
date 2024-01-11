402a9a35832a506475d0f9a14b4e67036e0816ac
joa <119975369+forjoa@users.noreply.github.com>
2024-01-11 13:33:15 +0100

    Update database.js

--- server/database/database.js
+++ server/database/database.js
@@ -3,8 +3,7 @@ import pg from 'pg'
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString: "postgres://default:Xkvi8BDNZf0u@ep-gentle-rain-15675697-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
-  
+  connectionString: "",
 })
   
 pool.connect((err) => {
