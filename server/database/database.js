b121ab404f04993593c0fa7b6381bb57b9f3a4e3
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-11 09:48:48 +0100

    Connection made to database

--- server/database/database.js
+++ server/database/database.js
@@ -3,5 +3,10 @@ import { Pool } from 'pg'
 const pool = new Pool({
   connectionString: import.meta.env.VITE_POSTGRES_URL + "?sslmode=require",
 })
+  
+pool.connect((err) => {
+  if (err) throw err
+  console.log('Connection made successful')
+})
 
 export default pool
