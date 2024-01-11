518de64a0bbe85ad137d5dfb9c047c76c664e437
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-11 19:17:12 +0100

    Register and login made correctly

--- server/api/users.js
+++ server/api/users.js
@@ -5,14 +5,44 @@ import { sql } from '@vercel/postgres'
 const users = express.Router()
 
 // register
-users.post('/login', async (req, res) => {
-    const { name, email, pwd } = req.body
+users.post('/register', async (req, res) => {
+  const { name, email, pwd } = req.body
 
+  const { rows } = await sql`SELECT * FROM users WHERE email = ${email}`
+
+  if (rows.length == 0) {
     const hashedPassword = await bcrypt.hash(pwd, 10)
 
-    const result = await sql`INSERT INTO users (name, email, pwd) VALUES (${name}, ${email}, ${hashedPassword})`
+    const result =
+      await sql`INSERT INTO users (name, email, pwd) VALUES (${name}, ${email}, ${hashedPassword})`
 
-    res.status(200).json(result)
+    if (result.rowCount > 0) {
+      res.status(200).send('user registered correctly')
+    } else {
+      res.status(400).send('something went wrong')
+    }
+  } else res.status(409).send('user already registered')
+})
+
+// login
+users.post('/login', async (req, res) => {
+  const { email, pwd } = req.body
+
+  const { rows } = await sql`SELECT * FROM users WHERE email = ${email}`
+
+  if (rows.length > 0) {
+    const pwdHashed = rows[0].pwd
+
+    const match = await bcrypt.compare(pwd, pwdHashed)
+
+    if (match) {
+      res.status(200).send('user logging correct')
+    } else {
+      res.status(401).send('incorrect password')
+    }
+  } else {
+    res.status(404).send('user not found')
+  }
 })
 
 export default users
