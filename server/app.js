c9d50a254558ab50c1fd777f38022aa8a4077174
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-11 18:08:13 +0100

    Port changed

--- server/app.js
+++ server/app.js
@@ -5,9 +5,11 @@ import clothes from './api/clothes.js'
 
 const app = express()
 
+
 app.use(cors())
 app.use(express.json())
 
+app.use('/', (req, res) => res.send('server running on port 5000'))
 app.use('/api/clothes', clothes)
 
-app.listen(3000, () => console.log('connected'))
+app.listen(5000, () => console.log('connected'))
