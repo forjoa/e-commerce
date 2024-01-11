7fc6ea27eb0c363bcda4809c0e6ea4bc8a3dc12d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-11 18:18:01 +0100

    Change name from app to index js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,9 @@ import clothes from './api/clothes.js'
 
 const app = express()
 
-
 app.use(cors())
 app.use(express.json())
 
-app.use('/', (req, res) => res.send('server running on port 5000'))
 app.use('/api/clothes', clothes)
 
 app.listen(5000, () => console.log('connected'))
