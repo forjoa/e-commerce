f36ade897b3bec04826ea328edba6b57e4098291
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-12 12:23:20 +0100

    Products shown on main screen

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,31 @@
+// imports
+import { useEffect, useState } from 'react'
+
+// types
+import { Product } from '../types/product.ts'
 
 const Main = () => {
-  const handle = async () => {
-    const response = await fetch('http://localhost:3000/api/clothes/allClothes')
-    console.log(response)
-  }
+  const [products, setProducts] = useState<Product[]>([])
 
-  handle()
+  useEffect(() => {
+    fetch('http://localhost:5000/api/clothes/allClothes')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+  }, [])
 
-  return <></>
+  return (
+    <>
+      <main className='main'>
+        {products.map((product) => {
+          return (
+            <div className="product" key={product.id}>
+              {product.name}
+            </div>
+          )
+        })}
+      </main>
+    </>
+  )
 }
 
 export default Main
---
+++ src/types/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number,
+    price: number,
+    size: string,
+    name: string,
+    available: boolean,
+    photo: string
+}
+
