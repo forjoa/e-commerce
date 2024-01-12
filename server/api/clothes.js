ddd45c46557a091ca11fb1d19628ac47c32c6201
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-12 09:10:17 +0100

    Add new clothes correctly made

--- server/api/clothes.js
+++ server/api/clothes.js
@@ -10,4 +10,17 @@ clothes.get('/allClothes', async (req, res) => {
     res.status(200).send(result.rows)
 })
 
+// add new clothes
+clothes.post('/addClothes', async (req, res) => {
+    const { price, size, name, available, photo } = req.body
+
+    const result = await sql`INSERT INTO clothes(price, size, name, available, photo) VALUES(${price}, ${size}, ${name}, ${available}, ${photo})`
+
+    if (result.rowCount > 0) {
+        res.status(200).send('clothes saved successfully')
+    } else {
+        res.status(400).send('something went wrong')
+    }
+})
+
 export default clothes 
