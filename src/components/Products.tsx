50efe23d569cd02fccd73905bd5ac40acb973d40
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-12 13:17:33 +0100

    All products shown correctly

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -18,7 +18,12 @@ const Products = () => {
       {products.map((product) => {
         return (
           <div className='product' key={product.id}>
-            {product.name}
+            <img src={product.photo} alt='Product image' />
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <button disabled={product.available ? false : true}>
+              Add to cart
+            </button>
           </div>
         )
       })}
