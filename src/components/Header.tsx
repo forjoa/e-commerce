ba95a83aa9fcc0a120f4f6e9bb49e72d3e06ef78
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-13 16:55:04 +0100

    Styles finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,17 @@ import '../styles/header.css'
 const Header = () => {
   return (
     <header className='home-header'>
-      <div className='header-left'>
-        <a href='/'>
-          <img src={logo} alt='Logo image' />
-          <h1>E-commerce</h1>
-        </a>
-      </div>
-      <div className='header-right'>
-        <button className='login-btn'>Login</button>
-        <button className='register-btn'>Register</button>
+      <div className='header-container'>
+        <div className='header-left'>
+          <a href='/'>
+            <img src={logo} alt='Logo image' />
+            <h1>E-commerce</h1>
+          </a>
+        </div>
+        <div className='header-right'>
+          <button className='login-btn'>Login</button>
+          <button className='register-btn'>Register</button>
+        </div>
       </div>
     </header>
   )
