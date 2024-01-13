66e841a91cbfd77826a9ac12530c79290c30cd5e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-13 10:46:34 +0100

    Created Header correctly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
+// images
+import logo from '../assets/icon.png'
 
 const Header = () => {
-    return (
-        <>
-        header
-        </>
-    )
+  return (
+    <header className='home-header'>
+      <div className='header-left'>
+        <a href='/'>
+          <img src={logo} alt='Logo image' />
+          <h1>E-commerce</h1>
+        </a>
+      </div>
+      <div className='header-right'>
+        <button className='login-btn'>Login</button>
+        <button className='register-btn'>Register</button>
+      </div>
+    </header>
+  )
 }
 
 export default Header
