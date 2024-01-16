ea27cbbc4899a784f3bfedac74f05ebb0b0bc8a7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-16 17:10:53 +0100

    Cart made correctly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 // images
 import logo from '../assets/icon.png'
-import { IconShoppingCart } from '@tabler/icons-react'
+import { IconShoppingCart, IconTrash } from '@tabler/icons-react'
 
 // styles
 import '../styles/header.css'
@@ -8,18 +8,25 @@ import '../styles/header.css'
 // imports
 import { cart } from '../utils/cart.ts'
 import { useEffect, useState } from 'react'
+import Popup from 'reactjs-popup'
+import { Product } from '../types/product.ts'
 
 const Header = () => {
-  const [cartCount, setCartCount] = useState<number>(cart.length) 
+  const [cartCount, setCartCount] = useState<number>(cart.length)
 
   useEffect(() => {
     setCartCount(cart.length)
-  }, []) 
+  }, [])
 
-  const showCart = () => {
-    console.log(cart)
+  // const showCart = () => {
+  //   console.log(cart)
+  // }
+
+  const deleteFromCart = (index : number) => (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    cart.splice(index, 1) 
   }
-  
+
   return (
     <header className='home-header'>
       <div className='header-container'>
@@ -32,7 +39,25 @@ const Header = () => {
         <div className='header-right'>
           <button className='login-btn'>Login</button>
           <button className='register-btn'>Register</button>
-          <IconShoppingCart onClick={showCart} />
+          <Popup trigger={<IconShoppingCart />} modal nested>
+            <div className='cart-list-container'>
+              {cart.map((product: Product, index: number) => {
+                return ( 
+                  <div className='product-cart' data-id={product.id}> 
+                    <img src={product.photo} alt='Product image' />
+                    <div className='product-cart-info'>
+                      <p>{product.name}</p>
+                      <span>{product.size}</span>
+                      <span>{product.price}</span>
+                    </div>
+                    <button value={index} onClick={deleteFromCart(index)}>
+                      <IconTrash color='red'/> 
+                    </button>
+                  </div>
+                )
+              })}
+            </div>
+          </Popup>
           <p className='cart-length'>{cartCount}</p>
         </div>
       </div>
