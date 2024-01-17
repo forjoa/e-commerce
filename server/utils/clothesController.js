f349c1d70bfa578988c5cb1c6de2d9fc84703600
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-17 10:28:15 +0100

    Change deconstructing

--- server/utils/clothesController.js
+++ server/utils/clothesController.js
@@ -3,36 +3,40 @@ import 'dotenv/config'
 
 export const clothesController = {
   getAll: async (req, res) => {
-    const result = await sql`SELECT * FROM clothes`
-    res.status(200).send(result.rows)
+    const { rows } = await sql`SELECT * FROM clothes`
+    res.status(200).send(rows)
   },
   addNew: async (req, res) => {
     const { price, size, name, available, photo } = req.body
 
-    const result =
+    const { rowCount } =
       await sql`INSERT INTO clothes(price, size, name, available, photo) VALUES(${price}, ${size}, ${name}, ${available}, ${photo})`
 
-    if (result.rowCount > 0) {
+    if (rowCount > 0) {
       res.status(200).json({ message: 'Clothes saved successfully' })
     } else {
-      res.status(400).send({ message: 'Something went wrong while saving clothes' })
+      res
+        .status(400)
+        .send({ message: 'Something went wrong while saving clothes' })
     }
   },
   delete: async (req, res) => {
     const { id } = req.body
 
-    const result = await sql`DELETE FROM clothes WHERE id = ${id}`
+    const { rowCount } = await sql`DELETE FROM clothes WHERE id = ${id}`
 
-    if (result.rowCount > 0) {
+    if (rowCount > 0) {
       res.status(200).json({ message: 'Clothes deleted successfully' })
     } else {
-      res.status(400).json({ message: 'Something went wrong while deleting clothes' })
+      res
+        .status(400)
+        .json({ message: 'Something went wrong while deleting clothes' })
     }
   },
   edit: async (req, res) => {
     const { id, price, size, name, available, photo } = req.body
 
-    const result = await sql`
+    const { rowCount } = await sql`
         UPDATE clothes
         SET price = ${price},
             size = ${size},
@@ -41,10 +45,12 @@ export const clothesController = {
             photo = ${photo}
         WHERE id = ${id}`
 
-    if (result.rowCount > 0) {
+    if (rowCount > 0) {
       res.status(200).json({ message: 'Clothes edited successfully' })
     } else {
-      res.status(400).json({ message: 'Something went wrong while editing clothes' })
+      res
+        .status(400)
+        .json({ message: 'Something went wrong while editing clothes' })
     }
   },
 }
